faa644885fed2f769005577c01adf54be1d5cac0
Kevin McPeak <kevmc46@gmail.com>
2022-08-27 16:58:54 -0400

    formatted skill icons

--- src/components/Background.js
+++ src/components/Background.js
@@ -2,35 +2,39 @@ import React from "react";
 
 let Background = () => {
 	return (
-		<div className="container-sm d-flex section ps-5 pe-5">
-			<h5 className="d-flex pe-5">Background</h5>
-			<div className="d-flex flex-column">
-				<p>
-					I have been interested in cutting edge technology ever since
-					I can remember. It took me until about 2 years ago to dive
-					in, and I haven't looked back! I can honestly say computer
-					programming is my life long passion. I love to build cool
-					stuff and solve problems! "Lorem ipsum dolor sit amet,
-					consectetur adipiscing elit, sed do eiusmod tempor
-					incididunt ut labore et dolore magna aliqua. Ut enim ad
-					minim veniam, quis nostrud exercitation ullamco laboris nisi
-					ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu
-					fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum."
-				</p>
+		<div className="container-sm section">
+			<div className="row">
+				<h5 className="col-3 ps-5">Background</h5>
+				<div className="col-7">
+					<p>
+						I have been interested in cutting edge technology ever
+						since I can remember. It took me until about 2 years ago
+						to dive in, and I haven't looked back! I can honestly
+						say computer programming is my life long passion. I love
+						to build cool stuff and solve problems! "Lorem ipsum
+						dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna
+						aliqua. Ut enim ad minim veniam, quis nostrud
+						exercitation ullamco laboris nisi ut aliquip ex ea
+						commodo consequat. Duis aute irure dolor in
+						reprehenderit in voluptate velit esse cillum dolore eu
+						fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+						non proident, sunt in culpa qui officia deserunt mollit
+						anim id est laborum."
+					</p>
 
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-					do eiusmod tempor incididunt ut labore et dolore magna
-					aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat.
-					Duis aute irure dolor in reprehenderit in voluptate velit
-					esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-					occaecat cupidatat non proident, sunt in culpa qui officia
-					deserunt mollit anim id est laborum.
-				</p>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+						sed do eiusmod tempor incididunt ut labore et dolore
+						magna aliqua. Ut enim ad minim veniam, quis nostrud
+						exercitation ullamco laboris nisi ut aliquip ex ea
+						commodo consequat. Duis aute irure dolor in
+						reprehenderit in voluptate velit esse cillum dolore eu
+						fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+						non proident, sunt in culpa qui officia deserunt mollit
+						anim id est laborum."
+					</p>
+				</div>
 			</div>
 		</div>
 	);
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,18 +4,22 @@ let Skills = (props) => {
 	// let el = <FontAwesomeIcon icon={BsGithub} />;
 	let allSkills = props.skills.map((item) => {
 		return (
-			<div className="row">
-				<div className="col-3">
+			<div>
+				<div className="m-3 d-flex flex-column">
 					<div>{item[0]}</div>
-					<div>{item[1]}</div>
+					<div className="text-center">{item[1]}</div>
 				</div>
 			</div>
 		);
 	});
 	return (
-		<div className="container-sm d-flex section ps-5 pe-5">
-			<h5 className="pe-5">Skills</h5>
-			<div>{allSkills}</div>
+		<div className="container-sm section">
+			<div className="row">
+				<h5 className="col-3 ps-5">Skills</h5>
+				<div className="col-7 d-flex flex-wrap justify-content-center">
+					{allSkills}
+				</div>
+			</div>
 		</div>
 	);
 };
