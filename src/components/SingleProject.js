c6eb0486cd3bdb776b8cc3cfdacd088b8dc9f1f2
Kevin McPeak <kevmc46@gmail.com>
2022-08-28 08:29:42 -0400

    updated format in project component

--- src/components/SingleProject.js
+++ src/components/SingleProject.js
@@ -2,20 +2,18 @@ import React from "react";
 
 let SingleProject = (props) => {
 	return (
-		<div className="container-sm">
+		<div className="container-sm section">
 			<div className="row">
 				<div className="col-4">
 					<h2>{props.title}</h2>
 					<div>project descriptions</div>
 				</div>
-				<div className="col-8">
-					<div className="project-image-container">
-						<img
-							className="testt"
-							src={props.projectPhoto}
-							alt="project photo"
-						/>
-					</div>
+				<div className="col-8 d-flex justify-content-center">
+					<img
+						className="project-image"
+						src={props.projectPhoto}
+						alt="project photo"
+					/>
 				</div>
 			</div>
 		</div>
