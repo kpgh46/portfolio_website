eb91a9f580a3f50eb4d51cb42eae073c2b437c00
Kevin McPeak <kevmc46@gmail.com>
2023-03-02 17:53:20 -0500

    fixed movie url

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -44,7 +44,7 @@ let Projects = () => {
 		your preferred category and learn more about
 		any type of movie. Built with NodeJS, MongoDB,
 		Express, and Pug.`,
-		liveUrl: "movieapp-production-7762.up.railway.app",
+		liveUrl: "https://movieapp-production-7762.up.railway.app",
 		codeUrl: "https://github.com/kpgh46/MovieApp",
 	};
 
